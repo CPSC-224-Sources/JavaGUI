6d3e145cbcb29c8eded9b72f589353da2e902c34
Aaron S. Crandall <crandall@gonzaga.edu>
2020-10-14 14:47:46 -0700

    Added a Python wx Hello World example

---
+++ PythonHelloWorld/PythonHelloWorldGUI.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import wx 
+ 
+app = wx.App() 
+window = wx.Frame(None, title = "wxPython Frame", size = (300,200)) 
+panel = wx.Panel(window) 
+label = wx.StaticText(panel, label = "Hello World", pos = (100,50)) 
+window.Show(True) 
+app.MainLoop()
