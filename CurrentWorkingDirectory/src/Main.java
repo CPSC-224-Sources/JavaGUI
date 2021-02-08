6df0b116a83e466638c100dbcdb4899331705f92
Aaron S. Crandall <crandall@gonzaga.edu>
2021-02-08 13:07:37 -0800

    Added working directory example

---
+++ CurrentWorkingDirectory/src/Main.java
@@ -0,0 +1,7 @@
+
+class Main {
+    public static void main(String args[]) {
+        System.out.println("running a demo");
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+    }
+}
