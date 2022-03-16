ceb269bf79a4f1d8814b8775220af92ecb8a8246
Aaron S. Crandall <crandall@gonzaga.edu>
2022-03-16 08:58:48 -0700

    Cleaned up button demo

--- ButtonExample/src/edu/gonzaga/ButtonExample.java
+++ ButtonExample/src/edu/gonzaga/ButtonExample.java
@@ -3,13 +3,19 @@ package edu.gonzaga;
 import javax.swing.*;
 public class ButtonExample {
     public static void main(String[] args) {
-        JFrame f=new JFrame("Button Example");
-        JButton b=new JButton("Click Here");
-        b.setBounds(50,100,95,30);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(b);
-        f.setSize(400,400);
-        f.setLayout(null);
-        f.setVisible(true); // STart mainloop
+        JFrame mainWindowFrame = new JFrame("Button Example");
+        JButton demoButton1 = new JButton("Click Here");
+
+        mainWindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainWindowFrame.add(demoButton1);
+
+        mainWindowFrame.pack();
+
+        mainWindowFrame.setVisible(true); // Start mainloop
     }
 }
+
+        // b.setBounds(50,100,95,30);
+        //f.setSize(400,400);
+        // f.setLayout(null);
