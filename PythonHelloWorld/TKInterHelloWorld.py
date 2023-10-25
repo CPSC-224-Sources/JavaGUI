4abd5bcc8741d9c4415a7b0e39edd1dae36524df
Aaron S. Crandall <crandall@gonzaga.edu>
2023-10-25 09:31:32 -0700

    Added a TKInter example

---
+++ PythonHelloWorld/TKInterHelloWorld.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Create a main window
+root = tk.Tk()
+
+# Create a label widget with "Hello, World!" text
+label = tk.Label(root, text="Hello, World!")
+
+# Pack the label into the main window
+label.pack()
+
+# Start the Tkinter main loop
+root.mainloop()
