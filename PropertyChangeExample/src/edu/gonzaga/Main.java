075b77bf3ee562ad578839f0491522200e1ea7f8
Aaron S. Crandall <crandall@gonzaga.edu>
2023-11-06 10:47:35 -0800

    Added more details to observer pattern demo

--- PropertyChangeExample/src/edu/gonzaga/Main.java
+++ PropertyChangeExample/src/edu/gonzaga/Main.java
@@ -11,8 +11,9 @@ public class Main {
         System.out.println(die);
 
         DieWatcher watcher1 = new DieWatcher(die);
+        DieWatcher watcher2 = new DieWatcher(die);
 
-        System.out.println("Rolling the die now");
+        System.out.println("Rolling the die now - all watchers shall be informed");
         die.roll();
         System.out.println("Die rolled a what? " + die.getValue());
 
